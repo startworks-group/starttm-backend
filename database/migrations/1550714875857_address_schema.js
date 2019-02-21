0425ca5849a4a9d3f9476af9db6d258b7ef2b4dc
Marlon Secundo <marlon_secundo@outlook.com>
2019-02-20 23:18:12 -0300

    Person and Address

---
+++ database/migrations/1550714875857_address_schema.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class AddressSchema extends Schema {
+  up() {
+    this.create('addresses', (table) => {
+      table.increments();
+      table.string('street').notNullable();
+      table
+          .integer('number')
+          .notNullable()
+          .unsigned();
+      table.string('neighborhood').notNullable();
+      table.string('cep').notNullable();
+      table.string('complement').notNullable();
+      table.string('city').notNullable();
+      table.string('state').notNullable();
+      table.unique([
+        'street',
+        'number',
+        'neightborhood',
+        'cep',
+        'complement',
+        'city',
+        'state',
+      ]);
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('addresses');
+  }
+}
+
+module.exports = AddressSchema;
