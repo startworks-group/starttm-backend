9ecddda8fb1c24d8a46ce29d0ba0846c5ff12fb1
John Victor <johnvictorio3@gmail.com>
2019-02-27 14:24:15 -0300

    email não pode ser .unique

--- database/migrations/1550754124027_subscription_schema.js
+++ database/migrations/1550754124027_subscription_schema.js
@@ -14,7 +14,6 @@ class SubscriptionSchema extends Schema {
       table
           .string('email', 254)
           .notNullable()
-          .unique();
       table.string('password', 60).notNullable();
       table
           .string('token', 16)
