db7974f139f8fb0fbf43f62ea771defe056e7266
johnalves <johnvictorio3@gmail.com>
2019-05-09 20:03:39 -0300

    (fix): roles

--- app/Controllers/Http/RoleController.js
+++ app/Controllers/Http/RoleController.js
@@ -1,14 +1,25 @@
 'use strict'
-const Role = use('App/Models')
+const { User} = use('App/Models')
+// TODO verificar se esta funcionando
+const Role = use('Role')
 
 class RoleController {
-    async createRole (){
-        const roleAdmin = new Role()
-        roleAdmin.name = 'Administrator'
-        roleAdmin.slug = 'Administrator'
-        roleAdmin.description = 'gerenciar privilegios administrativos'
-        await roleAdmin.save()
-    }
+   async createRole ({ request }) {
+     const { name, slug, description } = request.post();
+     const role = new Role();
+     role.name = name;
+     role.slug = slug;
+     role.description = description;
+     await role.save();
+     return role.toJSON();
+   }
+
+   async attachRole ({ request }) {
+     const { userId, roleId } = request.post()
+     const user = await User.find(userId)
+     await user.roles().attach([ roleId ])
+     return { result: 'ok' }
+   }
 }
 
 module.exports = RoleController
