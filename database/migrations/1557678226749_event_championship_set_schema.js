0412948f1f4826a8c6abee7b8aac25cc690a9a25
Marlon Secundo <marlon_secundo@outlook.com>
2019-05-12 14:10:24 -0300

    Set Migration Added

---
+++ database/migrations/1557678226749_event_championship_set_schema.js
@@ -0,0 +1,37 @@
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class SetSchema extends Schema {
+  up() {
+    this.create('sets', (table) => {
+      table.increments();
+      table
+        .integer('order')
+        .notNullable()
+        .unsigned();
+      table
+        .integer('playerOneScore')
+        .notNullable()
+        .unsigned();
+      table
+        .integer('playerTwoScore')
+        .notNullable()
+        .unsigned();
+      table
+        .integer('confront_id')
+        .notNullable()
+        .unsigned()
+        .references('confronts.id')
+        .onUpdate('cascade')
+        .onDelete('cascade');
+      table.timestamps();
+      table.unique(['order', 'confronts']);
+    });
+  }
+
+  down() {
+    this.drop('sets');
+  }
+}
+
+module.exports = SetSchema;
