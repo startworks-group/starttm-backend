5f36c532da5f9c39ab0307d4c775f02636e12533
Marlon Secundo <marlon_secundo@outlook.com>
2019-05-12 14:09:50 -0300

    Confront Migration Added

---
+++ app/Controllers/Http/Event/Championship/ConfrontController.js
@@ -0,0 +1,93 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with confronts
+ */
+class ConfrontController {
+  /**
+   * Show a list of all confronts.
+   * GET confronts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new confront.
+   * GET confronts/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new confront.
+   * POST confronts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single confront.
+   * GET confronts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing confront.
+   * GET confronts/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update confront details.
+   * PUT or PATCH confronts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a confront with id.
+   * DELETE confronts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = ConfrontController
---
+++ database/migrations/1557678155740_event_championship_confront_schema.js
@@ -0,0 +1,36 @@
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class ConfrontSchema extends Schema {
+  up() {
+    this.create('confronts', (table) => {
+      table.increments();
+      table
+        .integer('number')
+        .notNullable()
+        .unsigned();
+      table
+        .integer('playerOne')
+        .notNullable()
+        .unsigned();
+      table
+        .integer('playerTwo')
+        .notNullable()
+        .unsigned();
+      table
+        .integer('championship_id')
+        .notNullable()
+        .unsigned()
+        .references('championships.id')
+        .onUpdate('cascade')
+        .onDelete('cascade');
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('confronts');
+  }
+}
+
+module.exports = ConfrontSchema;
