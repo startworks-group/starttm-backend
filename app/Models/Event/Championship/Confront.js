d6148abaa3a65ce677a12b5bfaa9d6bb81e92367
Marlon Secundo <marlon_secundo@outlook.com>
2019-05-13 19:35:22 -0300

    Model Updated

--- app/Models/Event/Championship/Confront.js
+++ app/Models/Event/Championship/Confront.js
@@ -1,9 +1,26 @@
-'use strict'
-
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use('Model');
 
 class Confront extends Model {
+  static boot() {
+    super.boot();
+  }
+
+  static columns() {
+    return ['number', 'playerOne', 'playerTwo', 'arbiter', 'phase'];
+  }
+
+  playerOne() {
+    return this.belongsTo('App/Models/Athlete', 'playerOne', 'id');
+  }
+
+  playerTwo() {
+    return this.belongsTo('App/Models/Athlete', 'playerTwo', 'id');
+  }
+
+  championship() {
+    return this.belongsTo('App/Models/Championship');
+  }
 }
 
-module.exports = Confront
+module.exports = Confront;
