4d2c954e0341424ed92a6b760672569b2787be78
Marlon Secundo <marlon_secundo@outlook.com>
2019-06-05 20:14:21 -0300

    Acl Configs Updated

--- app/Models/User.js
+++ app/Models/User.js
@@ -19,10 +19,7 @@ class User extends Model {
   }
 
   static get traits() {
-    return [
-      '@provider:Adonis/Acl/HasRole',
-      '@provider:Adonis/Acl/HasPermission',
-    ]
+    return ['@provider:Adonis/Acl/HasRole', '@provider:Adonis/Acl/HasPermission'];
   }
 }
 
